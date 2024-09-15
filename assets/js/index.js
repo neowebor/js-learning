bc01114ae94282a6a727d80a80a8303713ea0813
neowebor <neowebor@gmail.com>
2024-09-15 17:43:18 +0300

    Add typeof examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,39 @@
 // тут можна писати Js код
 
-console.log('Hello there');
+// console.log('Hello there');
+
+// тип даних String
+'hello there'
+
+// Тип даних Number
+10;
+-50;
+7.23;
+0;
+
+// тип даних null
+null;
+
+// тип даних undefined
+undefined;
+
+// тип даних Symbol
+// new Symbol('приклад символу')
+
+//  Функція
+// alert();
+
+// let username1 = 'Hello there?'
+// console.log(username1);
+
+// username1 = 'Time is go'
+// console.log(username1);
+
+
+// const username2 = 'This is a const';
+// console.log(username2);
+
+// username2 = 'It is not working';
+// console.log(username2);
+
+
---
+++ assets/js/operators.js
@@ -0,0 +1,9 @@
+// оператори
+
+// typeof - оператор визначення типу даних
+
+typeof 10;
+
+let data = 'test';
+
+console.log(typeof data);
