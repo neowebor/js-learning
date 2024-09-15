52155ae0f741d1d9610d5291d2fbac2a28b6d67d
neowebor <neowebor@gmail.com>
2024-09-15 17:59:55 +0300

    Add basic math operators

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -7,3 +7,39 @@ typeof 10;
 let data = 'test';
 
 console.log(typeof data);
+
+let bug = null;
+console.log(typeof bug); // object
+
+// Математичні оператори
+// +, -, *, /,
+
+console.log('2 + 2');
+console.log(2 + 2);
+
+console.log('10 - 5');
+console.log(10 - 5);
+
+console.log('9 * 2');
+console.log(9 * 2);
+
+console.log('100 / 4'); 
+console.log(100 / 4);
+
+// Зведення в ступінь
+console.log(5 ** 3);
+
+console.log((-5) ** 2);
+
+// Асоціативність опріділяє порядок, в якому обрабатуються оператори з однаковим пріорітетом 
+
+// залишок від ділення
+
+console.log(-17%3);
+
+
+
+
+
+
+
