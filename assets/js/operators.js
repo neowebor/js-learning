5125e7b12c456c77585a878995f15403f1f8fea1
neowebor <neowebor@gmail.com>
2024-09-15 18:12:03 +0300

    Add prefix and postfix increment/decrement examples

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -37,7 +37,32 @@ console.log((-5) ** 2);
 
 console.log(-17%3);
 
+// інкрементатор і декрементатор
+let counter = 0;
 
+counter++; // 1..
+counter++;
+counter++;
+counter++;
+counter++; // ..5
+
+
+console.log('Мій лічильник ' + counter); // Мій лічильнік = 5
+
+counter--;
+console.log(counter); // 4
+
+// counter++ / counter-- - постфіксний
+// ++counter / --counter - префіксний
+
+let counter1 = 10;
+
+console.log(counter1++);
+
+
+let counter2 = 10;
+
+console.log(++counter2);
 
 
 
