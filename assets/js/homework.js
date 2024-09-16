3a579a8ddebbcc0f7327d08356899c9e5c624fb1
neowebor <neowebor@gmail.com>
2024-09-16 17:52:41 +0300

    Add homework operators solution

---
+++ assets/js/homework.js
@@ -0,0 +1,31 @@
+// Task 1
+let num1 = 2;
+let num2 = 4;
+
+let result1 = num1 + num2;
+
+console.log(result1);
+
+// Task 2
+let result2 = result1 - 50;
+console.log(result2);
+
+// Task 3
+let result3 = result1 * result2;
+console.log(result3);
+
+// Task 4
+let result4 = result3 / 2.687;
+console.log(result4);
+
+let reminderOfNumber = result3 % 2.687;
+console.log(reminderOfNumber);
+
+// Task 5
+let importantString = '' + reminderOfNumber;
+
+console.log('Ого, оце число.. ' + importantString);
+
+
+
+
