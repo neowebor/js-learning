fa3e17b0e8bd19ff55a0aa3dc5a300c8cdb595a8
neowebor <neowebor@gmail.com>
2024-09-17 20:16:11 +0300

    Add conditionals if .. else example

---
+++ assets/js/conditionals.js
@@ -0,0 +1,26 @@
+// умовні конструкції
+// виконувати певні дії за певних умов
+
+console.log('1');
+
+/* 
+  if ( умова ) {
+    // дія
+  }
+
+  дія якак буде виконана тільки тоді, коли умова буде правдиваю
+*/
+// let text = prompt('Write yes to run блок коду 2');
+// if (text === 'yes') {
+//   console.log('2');
+// }
+
+// console.log('3');
+let number = +prompt('Enter number')
+
+if (number > 0) {
+  console.log('Number is positive');
+} else {
+  console.log('Number is negative');
+}
+
