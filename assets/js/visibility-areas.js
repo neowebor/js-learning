709168c45b656e999aa38b352f53ff082ae8d561
neowebor <neowebor@gmail.com>
2024-09-19 19:33:55 +0300

    Add js scope

---
+++ assets/js/visibility-areas.js
@@ -0,0 +1,15 @@
+// області видимості
+
+// позначається {}
+
+// let number = 10;
+
+const testFunc = function() {
+  let number = 5;
+  console.log(number);
+  
+}
+
+testFunc();
+
+console.log(number);
