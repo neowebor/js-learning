2800d1a165d52c7680953105f8e48443708de4e0
neowebor <neowebor@gmail.com>
2024-09-29 14:50:57 +0300

    Add array creation example

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -6,13 +6,42 @@ const vipEmails = new Array(
 );
 
 
-console.log(typeof vipEmails); // масив - це варіація об'єкта (object)
+// console.log(typeof vipEmails); // масив - це варіація об'єкта (object)
 
-console.log(vipEmails);
-console.log(vipEmails.length);
+// console.log(vipEmails);
+// console.log(vipEmails.length);
 
 // елемент з індексом 2
-console.log(vipEmails[2]);
+// console.log(vipEmails[2]);
+
+
+// Створенння масивів
+
+// 1. Через конструктор
+// пустий масив
+const arr1 = new Array();
+
+// масив з якимись елементами
+const arr2 = new Array(50, 515, 8478, 4456);
+
+// пустий масив з певною довжиною
+const arr3 = new Array(5);
+
+const users = new Array(
+  'Kirill',
+  'Alexandr',
+  'Viktor'
+);
+
+
+// 2. Через літерал
+// пустий масив
+const arr4 = [];
+
+// масив з якимись елементами
+const arr5 = [50, 515, 8478, 4456];
+
+const users2 = ['Lesha', 'Anton', 'Maxim'];
 
 
 
