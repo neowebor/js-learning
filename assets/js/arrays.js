688ca00a03816364119953e5eb6f04cccda39b34
neowebor <neowebor@gmail.com>
2024-09-29 14:43:25 +0300

    Add arrays example

---
+++ assets/js/arrays.js
@@ -0,0 +1,19 @@
+const vipEmails = new Array(
+  'asdqwe@test',
+  'asdfgbch@test',
+  '1234tgdhhxc@test',
+  '67fhfjha234@test'
+);
+
+
+console.log(typeof vipEmails); // масив - це варіація об'єкта (object)
+
+console.log(vipEmails);
+console.log(vipEmails.length);
+
+// елемент з індексом 2
+console.log(vipEmails[2]);
+
+
+
+
