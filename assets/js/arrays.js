2ce3e6c809586afe559b7748849072b3fccce330
neowebor <neowebor@gmail.com>
2024-09-30 20:04:20 +0300

    Add map example

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -1,10 +1,9 @@
 const vipEmails = new Array(
-  'asdqwe@test',
-  'asdfgbch@test',
-  '1234tgdhhxc@test',
-  '67fhfjha234@test'
+  "asdqwe@test",
+  "asdfgbch@test",
+  "1234tgdhhxc@test",
+  "67fhfjha234@test"
 );
-
 
 // console.log(typeof vipEmails); // масив - це варіація об'єкта (object)
 
@@ -13,7 +12,6 @@ const vipEmails = new Array(
 
 // елемент з індексом 2
 // console.log(vipEmails[2]);
-
 
 // Створенння масивів
 
@@ -27,12 +25,7 @@ const arr2 = new Array(50, 515, 8478, 4456);
 // пустий масив з певною довжиною
 const arr3 = new Array(5);
 
-const users = new Array(
-  'Kirill',
-  'Alexandr',
-  'Viktor'
-);
-
+const users = new Array("Kirill", "Alexandr", "Viktor");
 
 // 2. Через літерал
 // пустий масив
@@ -41,29 +34,27 @@ const arr4 = [];
 // масив з якимись елементами
 const arr5 = [50, 515, 8478, 4456];
 
-const users2 = ['Lesha', 'Anton', 'Maxim'];
+const users2 = ["Lesha", "Anton", "Maxim"];
 
 arr5[4] = Infinity;
 
 // arr5[7] = NaN;
 
 // додавання елементів у кінець массиву
-arr5.push('push1', 'push2', 'push3');
+arr5.push("push1", "push2", "push3");
 arr5.push(null, {}, 67, undefined);
 
 const newArray5Length = arr5.push(1500000);
 // console.log(newArray5Length);
 
-
 // видалення останнього елемента масиву
 const deletedValue = arr5.pop();
 
-users.push('EREN EGER');
-users.push('MIKASA AKERMAN', 'ARMIN');
+users.push("EREN EGER");
+users.push("MIKASA AKERMAN", "ARMIN");
 
 const lastDeletedName = users.pop();
 // console.log(lastDeletedName);
-
 
 // Методи масива ------------------------------------------------------------
 
@@ -77,21 +68,20 @@ const isVipUsersArray = Array.isArray(users);
 // console.log(arr5);
 
 arr5.unshift({
-  test: true
-})
+  test: true,
+});
 
 // console.log(arr5);
 
-arr5.unshift(-1000, 'test 12323', null);
+arr5.unshift(-1000, "test 12323", null);
 
 // console.log(arr5);
 
-users.unshift('Violetta', 'Viktoria')
+users.unshift("Violetta", "Viktoria");
 // console.log(users);
 
 const firstDeletedName = users.shift();
 // console.log(firstDeletedName);
-
 
 // видалення та вставка елементів у будь-яку точку масиву
 /* 
@@ -112,7 +102,7 @@ const firstDeletedName = users.shift();
   другий параметр - вказує перед яким індексом треба зупинитися
 */
 
-// console.log(arr5); 
+// console.log(arr5);
 
 const copy = arr5.slice(3, 7);
 
@@ -127,7 +117,7 @@ const copy3 = arr5.slice(-4, -2);
 
 const copy4 = arr5.slice(1, 5);
 
-copy4[2].newProp = 'wifi';
+copy4[2].newProp = "wifi";
 
 // метод join
 
@@ -137,9 +127,93 @@ copy4[2].newProp = 'wifi';
   або рядком який передали як параметр
 */
 
-const joinTest = ['Name 1', 'Name 2', true, 1000];
+const joinTest = ["Name 1", "Name 2", true, 1000];
 
 const stringFromArr = joinTest.join();
+
+// обертання елементів у масиві
+const reverseArr = [100, 5000, -1, -2];
+reverseArr.reverse();
+
+// includes - перевіряє чи є якийсь елемент у масиві
+// console.log(users.includes("Kirill")); // true
+
+// flat
+// створює масив всередені якого розрівнює вкладені масиви
+// на вказану кількість рівнів
+const onionArr = [
+  "first",
+  "second",
+  [100, 200, 300],
+  "third",
+  [true, true, false, ["deep stuff"], false],
+];
+
+// console.log(onionArr[onionArr.length - 1][3][0]);
+const flatted1 = onionArr.flat(1);
+// console.log(flatted1);
+
+
+/*
+  Методи обходу масиву
+
+  завжди обходять масив з початку до кінця
+  кожен з методів обходу буде приймати спеціальну функцію, яку називають callback (або функція зворотнього виклику)
+*/
+
+function printSomeData(printFunction, dataToPrint) {
+  printFunction(dataToPrint);
+};
+
+// printSomeData(confirm, 'test');
+// printSomeData(console.log, 'test');
+
+
+// forEach - виконує для кожного елемента масива callback функцію
+/* 
+  function callback(currentValue, index, array) {
+    currentValue - поточний елемент масиву
+    index - індекс поточного елементу масива
+    array - посилання на масив по якому ви робите обхід
+  }
+*/
+const forEachTestArray = [1, 2, 3, 4, 5];
+
+// function forEachCallbackExample(currentValue, index, array) {
+//   console.log('current value is');
+//   console.log(currentValue);
+  
+//   console.log('index is');
+//   console.log(index);
+  
+//   console.log('array value is');
+//   console.log(array);
+  
+// }
+
+// forEachTestArray.forEach(function (elem) {
+//   console.log(elem);
+// });
+
+// map - на основі результатів роботи коллбек-функції
+// та старого масиву створює новий масив
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const newArray = [];
+
+numbers.forEach(function(number) {
+  newArray.push(number * number);
+});
+
+// console.log(newArray);
+
+const squaredNumbers = numbers.map(function(number) {
+  // результат роботи коллбеку буде кластися у новий масив на поточний індекс, на якому зараз знаходиться обхід
+  return number * number;
+});
+
+console.log(squaredNumbers);
 
 
 
