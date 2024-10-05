93e3975a7203109208376f9c94ee051d2936e465
neowebor <neowebor@gmail.com>
2024-10-05 14:55:19 +0300

    Add advanced try..catch example

--- assets/js/errors.js
+++ assets/js/errors.js
@@ -22,25 +22,47 @@
 // console.log('весь код виконано');
 
 
-try {
-  console.log(1)
+// try {
+//   console.log(1)
 
-  const err1 = new SyntaxError('This is syntax error');
+//   const err1 = new SyntaxError('This is syntax error');
 
-  err1.status = 500;
+//   err1.status = 500;
 
-  throw err1;
+//   throw err1;
 
-  console.log(2)
-} catch(err) {
+//   console.log(2)
+// } catch(err) {
 
-  console.log(err.message);
-  console.log(err.status);
+//   console.log(err.message);
+//   console.log(err.status);
 
-} finally {
-  // блок коду який виконується або після catch або після повного виконання try
-  console.log('finally');
+// } finally {
+//   // блок коду який виконується або після catch або після повного виконання try
+//   console.log('finally');
   
+// }
+
+// console.log('Помилку оброблено');
+
+
+function getSumOfTwoNumbers(num1, num2) {
+  if(typeof num1 !== 'number' || isNaN(num1)) {
+    throw new TypeError('num1 має бути корректним числом')
+  }
+
+  if(typeof num2 !== 'number' || isNaN(num2)) {
+    throw new TypeError('num2 має бути корректним числом')
+  }
+
+  return num1 + num2;
 }
 
-console.log('Помилку оброблено');
+try {
+  const res = getSumOfTwoNumbers(NaN, '2');
+  console.log(res);
+
+} catch (err) {
+  console.log(err.message)
+}
+
