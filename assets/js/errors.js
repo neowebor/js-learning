dca1cfd8d857d9048a3c11d3e49720e628d91bc7
neowebor <neowebor@gmail.com>
2024-10-05 14:47:34 +0300

    Add try..catch..finally example

--- assets/js/errors.js
+++ assets/js/errors.js
@@ -1,10 +1,46 @@
+
+// try {
 // console.log(1);
 
-// const err1 = new Error('Приклад опису помилки');
+// // const err1 = new Error('Приклад опису помилки');
 
-// err1.status = 500;
+// // err1.status = 500;
 
-throw new Error('Test');
+// throw new Error('Test');
 
-console.log(2);
+// console.log(2);
+// } catch (err) {
+//   // блок коду який виконається якщо у блоці try відбулася помилка
+//   console.log('помилка сталася')
+//   console.dir(err);
+  
+//   if(err.message === 'Test') {
+//     // alert('обробив')
+//   }
+// }
 
+// console.log('весь код виконано');
+
+
+try {
+  console.log(1)
+
+  const err1 = new SyntaxError('This is syntax error');
+
+  err1.status = 500;
+
+  throw err1;
+
+  console.log(2)
+} catch(err) {
+
+  console.log(err.message);
+  console.log(err.status);
+
+} finally {
+  // блок коду який виконується або після catch або після повного виконання try
+  console.log('finally');
+  
+}
+
+console.log('Помилку оброблено');
