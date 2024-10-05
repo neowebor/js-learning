a7d35abb987940ef915edcfefcc06fdcf835de45
neowebor <neowebor@gmail.com>
2024-10-05 15:29:27 +0300

    Add primitive properties examples

---
+++ assets/js/primitives.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// console.log('test text');
+
+const textFromServer = 'test text';
+
+// console.log(textFromServer);
+
+const str1 = 'test';
+console.log(str1);
+
+const str2 = new String('test');
+console.log(str2);
+
+const bool2 = new Boolean(false);
+
+if(bool2) {
+  console.log(1);
+}
+
