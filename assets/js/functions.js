cb1879ffabe6e35f63584fe001a149d4758c7af5
neowebor <neowebor@gmail.com>
2024-10-10 18:05:02 +0300

    Add arrow function examples

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,7 +6,6 @@
 // let text = 'test';
 // console.log(text, 123, 'Hello');
 
-
 // let username;
 // username = prompt('Enter your name');
 
@@ -26,26 +25,24 @@
 
 // alert(num1 + num2)
 
-
 // Створення функцій
-
 
 // function expression (функціональний вираз)
 //const getSumOfTwoNumbers = function(firstNumber, secondNumber) {
-  // тіло функції - блок коду який буде виконано коли функція запуститься
-  // console.log('Hello from function');
-  // console.log('You can do anything you want here'); 
-  // console.log('Ваше значення: ' + firstParam);
+// тіло функції - блок коду який буде виконано коли функція запуститься
+// console.log('Hello from function');
+// console.log('You can do anything you want here');
+// console.log('Ваше значення: ' + firstParam);
 
-  // const firstNumber = +prompt("Enter first number");
-  // const secondNumber = +prompt("Enter second number");
+// const firstNumber = +prompt("Enter first number");
+// const secondNumber = +prompt("Enter second number");
 
-  // const sumResult = firstNumber + secondNumber;
+// const sumResult = firstNumber + secondNumber;
 
-  // console.log(firstNumber + secondNumber);
+// console.log(firstNumber + secondNumber);
 
-  // return каже функції її підсумковий результат роботи
-  // Після першого виконаного рядка з return функція зупиняє свою роботу
+// return каже функції її підсумковий результат роботи
+// Після першого виконаного рядка з return функція зупиняє свою роботу
 
 //   return sumResult;
 // }
@@ -62,13 +59,10 @@
 // myIDontKnowThisNumberFunction(10);
 // myIDontKnowThisNumberFunction()
 
-
-
-
 //function declaration
 // function myFunc() {
 //   console.log('123');
-  
+
 // }
 
 // Чисті функції
@@ -91,14 +85,13 @@ function badSum(num1, num2) {
 
 const res2 = badSum(2, 2);
 
-
-// 
+//
 function multiply(num1, num2) {
   return num1 * num2;
 }
 
 function getSquare(num) {
-  return multiply(num, num)
+  return multiply(num, num);
 }
 
 // Функції вищого порядку (HOF - high order function)
@@ -109,7 +102,6 @@ function getSquare(num) {
 */
 
 // forEach - функція вищого порядку тому що приймає іншу функцію
-
 
 //
 
@@ -123,16 +115,92 @@ function getSquare(num) {
 
 // const result2 = hofExample2();
 
-function sayHello(userName = 'Guest') {
+function sayHello(userName = "Guest") {
   return `Hello ${userName}`;
 }
 
-const hello1 = sayHello('John');
+const hello1 = sayHello("John");
 console.log(hello1);
 
 const hello2 = sayHello();
 console.log(hello2);
 
 /**
- * 
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
  */
+function declarationSum(num1, num2) {
+  return num1 + num2;
+}
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+const expressionSum = function (num1, num2) {
+  return num1 + num2;
+};
+
+// arrow function (функції - стрілки)
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+const arrowSum = (num1, num2) => {
+  return num1 + num2;
+};
+
+// 1. У них існує скорочена версія
+const arrowSumCompact = (num1 = 2, num2 = 4) => num1 + num2;
+
+// 1.5 особливості запису дужок параметрів
+const arrow1 = (param) => console.log(param);
+
+// 2. відсутній arguments
+function test() {
+  console.log(arguments);
+}
+
+const arrowTest = () => {
+  console.log(arguments); // error
+};
+
+// 3. Не можуть використовуватися як конструктори
+
+// 4. У стрілок відсутній власний this
+// стрілки беруть значення this з батьківського контексту виконання
+
+const arrowThis = () => {
+  console.log(this);
+};
+
+const eShopObject = {
+  name: "єШоп",
+  email: "admin@eshop.com",
+  products: [
+    { name: "Prod 1", price: 1000, quantity: 5 },
+    { name: "Prod 2", price: 269, quantity: 5000 },
+    { name: "Prod 3", price: 1233, quantity: 52323 },
+    { name: "Prod 4", price: 222222, quantity: 877 },
+    { name: "Prod 5", price: 800, quantity: 100 },
+  ],
+  logAllProducts: function() {
+
+    this.products.forEach(function (product) {
+      console.log(`Магазин ${this.name} продає на розетці товар з назвою ${product.name} і ціною ${product.price} `)
+    });
+  },
+  logAllProducts2: function() {
+
+    const callbackWithBadThis = function(product) {
+      console.log(`Магазин ${this.name} продає на розетці товар з назвою ${product.name} і ціною ${product.price} `)
+    };
+
+    const callback = callbackWithBadThis.bind(this);
+
+    this.products.forEach(callback);
+  }
+};
