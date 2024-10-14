2c49514ff6b5948493ece7242d096c62ba23ae83
neowebor <neowebor@gmail.com>
2024-10-14 18:33:33 +0300

    Add closures examples

--- assets/js/closures.js
+++ assets/js/closures.js
@@ -1,67 +1,67 @@
 'use strict';
-let data = 'letter';
+// let data = 'letter';
 
-function log1() {
-  console.log(`log1: ${data}`);
-}
+// function log1() {
+//   console.log(`log1: ${data}`);
+// }
 
-log1(); // letter
+// log1(); // letter
 
-function log2() {
-  let data = 151;
-  console.log(`log2 outer: ${data}`);
-}
+// function log2() {
+//   let data = 151;
+//   console.log(`log2 outer: ${data}`);
+// }
 
-log2(); 
+// log2(); 
 
-function log3() {
-  let data = NaN;
+// function log3() {
+//   let data = NaN;
 
-  console.log(`log3: ${data}`); // NaN
+//   console.log(`log3: ${data}`); // NaN
   
-  log1();
+//   log1();
 
-  if(true) {
-    let data = true;
-    console.log(`log3 inner: ${data}`); // true
-  }
-}
-
-
-log3(); 
+//   if(true) {
+//     let data = true;
+//     console.log(`log3 inner: ${data}`); // true
+//   }
+// }
 
 
-// Лексичне оточення функції - спеціальний об'єкт, який містить всі змінні, які функція бачить. Розраховується на момент створення функції
+// log3(); 
 
-function log4() {
-  let data = NaN;
 
-  function innerLog() {
-    console.log(`innerLog: ${data}`);
-  }
+// // Лексичне оточення функції - спеціальний об'єкт, який містить всі змінні, які функція бачить. Розраховується на момент створення функції
 
-  function innerInnerLog() {
-    let data = 123;
+// function log4() {
+//   let data = NaN;
 
-    innerLog();
-  }
+//   function innerLog() {
+//     console.log(`innerLog: ${data}`);
+//   }
 
-  innerLog();
-  innerInnerLog();
-}
+//   function innerInnerLog() {
+//     let data = 123;
 
-log4();
+//     innerLog();
+//   }
 
-function createGreeting(name) {
+//   innerLog();
+//   innerInnerLog();
+// }
 
-  function greet() {
-    return `Hello, ${name}`;
-  }
+// log4();
 
-  return greet;
-}
+// function createGreeting(name) {
 
-const greetAnton = createGreeting('Anton');
+//   function greet() {
+//     return `Hello, ${name}`;
+//   }
+
+//   return greet;
+// }
+
+// const greetAnton = createGreeting('Anton');
 
 function createId () {
   let currentId = 0;
@@ -84,5 +84,52 @@ function createId () {
 }
 
 const userIdHelper = createId();
+
+
+function createMultiplier (x) {
+  let res = 1; 
+
+  function multiply(y) {
+    res *= y; 
+    return res * x;
+  }
+
+  return multiply;
+}
+
+const multiplyResult = createMultiplier(5);
+
+// console.log(multiplyResult(2)); // 2 * 5 = 10
+// console.log(multiplyResult(2)); // 20
+// console.log(multiplyResult(100)); // 2000
+
+function inBetween(a, b) {
+  return function(x) {
+    return x >= a && x <= b;
+  };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert( arr.filter(inBetween(3, 6)) );
+
+// Смотри на то, что у тебя inBetween возвращает функцию и эта функция вызывается для каждого элемента в фильтре (так работает filter)
+
+
+
+// ВАЖНО. Обратите внимание — когда функция выполняется, её контекст выполнения удаляется из стека, но её лексическое окружение может или не может быть удалено из памяти, в зависимости от того, ссылается ли на это лексическое окружение другое лексическое окружение.
+
+/* 
+  personLexicalEnvironment = {
+  environmentRecord: {
+    name : 'Peter',
+    displayName: < displayName function reference>
+  }
+  outer: <globalLexicalEnvironment>
+}
+
+Когда функция person завершится, её контекст выполнения выкинется из стека. Но её лексическое окружение всё ещё останется в памяти, так как на него ссылается лексическое окружение его внутренней функции displayName. Таким образом, её переменные всё ещё будут доступны в памяти.
+*/
+
+// Замыкание это способ получения доступа и управления внешними переменными из функции.
 
 
