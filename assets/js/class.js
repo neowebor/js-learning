5b135f0f2447d470f5516a2b1704741c9101a740
neowebor <neowebor@gmail.com>
2024-10-15 13:44:45 +0300

    Update something

--- assets/js/class.js
+++ assets/js/class.js
@@ -50,3 +50,33 @@ class House {
 
 const house3 = new House();
 const house4 = new House(4, 4, 'test street 4', 10000);
+
+
+class Product {
+  constructor (productName, price, totalQuantityOnStock = 100, isForAdults) {
+    this.productName = productName;
+    this.price = price;
+    this.totalQuantityOnStock = totalQuantityOnStock;
+    this.isForAdults = isForAdults;
+  }
+
+  getTotalProductSum () {
+    return this.totalQuantityOnStock * this.price;
+  }
+}
+
+const phones = new Product('Galaxy s10', 12000, 20, false);
+console.log(phones.getTotalProductSum())
+
+const owner1 = {
+  name: 'Owner 1',
+  email: 'owner1@gmail.com',
+  balance: 5000
+}
+
+//
+// if(owner1.balance >= (phones.price * phones.totalQuantityOnStock) ) {
+//   // alert('Owner 1 може купити все морозиво')
+// } else {
+//   // alert('Owner 1 не може купити все морозиво, бо йому бракує коштів')
+// }
